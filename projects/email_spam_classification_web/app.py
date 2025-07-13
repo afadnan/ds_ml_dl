e8f66277c8be5ce9abc5a95dc9011db65cbadbb7
Adnan Ashraf <af.adnan23@gmail.com>
2025-07-13 22:45:03 +0530

    structure the code

--- projects/email_spam_classification_web/app.py
+++ projects/email_spam_classification_web/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
@@ -32,28 +32,23 @@ def transform_text(text):
 
     return " ".join(y)
 
-
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email Spam Classifier")
 
+input_sms = st.text_area("Enter the message")
+
 if st.button('Predict'):
 
     # 1. preprocess
     transformed_sms = transform_text(input_sms)
-
     # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-
     # 3. predict
     result = model.predict(vector_input)[0]
-
-    # 4.Display
+    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
         st.header("Not Spam")
-
-
-
